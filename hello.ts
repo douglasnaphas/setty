fc84c72750ef0502e0e67027da990839217d439e
Douglas Naphas <douglasnaphas@gmail.com>
2023-08-06 23:29:56 -0400

    Make an incomprehensible change due to moduling
    I get an error:
    
    Cannot redeclare block-scoped variable 'name'.ts(2451)
    lib.dom.d.ts(27029, 15): 'name' was also declared here.
    
    that goes away when I add
    
    export {}
    
    https://stackoverflow.com/questions/35758584/cannot-redeclare-block-scoped-variable

--- hello.ts
+++ hello.ts
@@ -1,5 +1,6 @@
 #!/usr/bin/env npx ts-node
-import { program } from "commander";
+// import { program } from "commander";
+const program = require("commander");
 program.option("--name");
 
 program.parse();
@@ -8,3 +9,5 @@ const options = program.opts();
 const name: string = options.name || "World";
 
 console.log(`Hello, ${name}`);
+
+export {};
