824185030edce22cf2e1b447d1ba198122c6190e
Douglas Naphas <douglasnaphas@gmail.com>
2023-08-07 15:13:30 -0400

    Use module.exports, add tests
    I got this error:
    
    https://stackoverflow.com/questions/38296667/getting-unexpected-token-export
    
    when trying to consume the exports.

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
-export { greeting } from "./lib/greeting";
+// export { greeting } from "./lib/greeting";
+import { greeting } from "./lib/greeting";
+import { greeting2 } from "./lib/greeting2";
+module.exports = { greeting, greeting2 };
---
+++ lib/greeting2.ts
@@ -0,0 +1,3 @@
+export function greeting2(name: string) {
+  return `Hi, ${name}`;
+}
