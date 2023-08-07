19ce43dd90de0342e93235e19a4f75cb32939d71
Douglas Naphas <douglasnaphas@gmail.com>
2023-08-07 16:17:26 -0400

    Parse name as an option with a value

--- hello.ts
+++ hello.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env npx ts-node
 const program = require("commander");
 const greeting = require(".").greeting;
-program.option("--name");
+program.option("--name <name>", "who to greet", "World");
 
 program.parse();
 
 const options = program.opts();
-const name: string = options.name || "World";
+const name: string = options.name;
 
 console.log(greeting(name));
 
